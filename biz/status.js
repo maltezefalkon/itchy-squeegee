f59a15a9740c24a3a661e28e0537f4c5b66546b3
maltezefalkon <maltezefalkon@gmail.com>
2015-10-28 10:51:29 -0400

    Added contact and about pages

--- biz/status.js
+++ biz/status.js
@@ -1,7 +1,8 @@
-﻿function StatusEnum(id, description, descriptionTemplate, glyphicon, colorClass, isOK, isBegun) {
+﻿function StatusEnum(id, description, descriptionTemplate, explanation, glyphicon, colorClass, isOK, isBegun) {
     this.StatusID = id;
     this.Description = description;
     this.DescriptionTemplate = descriptionTemplate;
+    this.Explanation = explanation;
     this.Glyphicon = glyphicon;
     this.ColorClass = colorClass;
     this.IsOK = isOK;
@@ -30,10 +31,10 @@ SubmissionStatusEnum.prototype.constructor = SubmissionStatusEnum;
 
 // ------------------------------------------------------------------------------------ v
 var DocumentStatus = {
-    Missing: new DocumentStatusEnum(10, 'Missing', 'Missing', 'glyphicon-remove', 'danger', false, false),
-    Valid: new DocumentStatusEnum(100, 'Valid', 'Valid', 'glyphicon-star', 'primary', false, true, false),
-    Expired: new DocumentStatusEnum(50000, 'Expired', 'Expired', 'glyphicon-time', 'danger', false, true),
-    Error: new DocumentStatusEnum(100000, 'Error', 'Error', 'glyphicon-remove-circle', 'danger', false, true),
+    Missing: new DocumentStatusEnum(10, 'Missing', 'Missing', 'This document has not yet been completed.', 'glyphicon-remove', 'danger', false, false),
+    Valid: new DocumentStatusEnum(100, 'Valid', 'Valid', 'This document has been saved to your account and is still valid.', 'glyphicon-star', 'primary', false, true, false),
+    Expired: new DocumentStatusEnum(50000, 'Expired', 'Expired', 'This document has been saved to your account, but is no longer valid.', 'glyphicon-time', 'danger', false, true),
+    Error: new DocumentStatusEnum(100000, 'Error', 'Error', 'There has been an error processing this document.', 'glyphicon-remove-circle', 'danger', false, true),
     LookupByID: function (id) {
         for (var s in DocumentStatus) {
             if (DocumentStatus[s].StatusID == id) {
@@ -68,13 +69,13 @@ var DocumentStatus = {
 };
 
 var SubmissionStatus = {
-    Missing: new SubmissionStatusEnum(10, 'Missing', 'Missing', 'glyphicon-remove', 'danger', false, false, false),
-    Created: new SubmissionStatusEnum(100, 'Not Submitted', 'Not Submitted', 'glyphicon-exclamation-sign', 'danger', false, true, false),
-    AwaitingApproval: new SubmissionStatusEnum(1000, 'Awaiting Approval', 'Awaiting Approval', 'glyphicon-hourglass', 'primary', false, true, true),
-    Error: new SubmissionStatusEnum(2000, 'Error', 'Error', 'glyphicon-remove-circle', 'danger', false, true, false),
-    Rejected: new SubmissionStatusEnum(3000, 'Rejected', 'Rejected', 'glyphicon-ban-circle', 'danger', false, true, false),
-    Expired: new SubmissionStatusEnum(4000, 'Expired', 'Expired', 'glyphicon-time', 'warning', false, true, true),
-    Approved: new SubmissionStatusEnum(5000, 'Approved', 'Approved', 'glyphicon-ok', 'success', true, true, true),
+    Missing: new SubmissionStatusEnum(10, 'Missing', 'Missing', 'This document has not yet been completed.', 'glyphicon-remove', 'danger', false, false, false),
+    Created: new SubmissionStatusEnum(100, 'Not Submitted', 'Not Submitted', 'This document has been completed but has not yet been sent to this organization.', 'glyphicon-exclamation-sign', 'danger', false, true, false),
+    AwaitingApproval: new SubmissionStatusEnum(1000, 'Awaiting Approval', 'Awaiting Approval', 'This document has been submitted to this organization and is pending approval by that organization.', 'glyphicon-hourglass', 'primary', false, true, true),
+    Error: new SubmissionStatusEnum(2000, 'Error', 'Error', 'There has been an error processing this document.', 'glyphicon-remove-circle', 'danger', false, true, false),
+    Rejected: new SubmissionStatusEnum(3000, 'Rejected', 'Rejected', 'The submitted document was rejected by the organization it was submitted to.', 'glyphicon-ban-circle', 'danger', false, true, false),
+    Expired: new SubmissionStatusEnum(4000, 'Expired', 'Expired', 'The document is no longer valid and needs to be renewed.', 'glyphicon-time', 'warning', false, true, true),
+    Approved: new SubmissionStatusEnum(5000, 'Approved', 'Approved', 'The document is completed and approved by the organization.', 'glyphicon-ok', 'success', true, true, true),
     LookupByID: function (id) {
         for (var s in SubmissionStatus) {
             if (SubmissionStatus[s].StatusID == id) {
