3f57008f3e207984bf6ec0535f7ddba01d983142
maltezefalkon <maltezefalkon@gmail.com>
2015-11-01 09:17:06 -0500

    Added instructions to educator tenure page

--- app/views/EducatorTenure.data.js
+++ app/views/EducatorTenure.data.js
@@ -6,6 +6,9 @@ module.exports = function (req) {
     var data = new ViewData(req, 'Enter Employment History', 'EducatorTenure');
     data.forceCurrent = !Boolean(req.user.InvitationID);
     data.pageMasthead = data.generatePageMasthead(data.forceCurrent ? 'Current Employment' : 'Employment History');
+    data.pageInstructions = data.forceCurrent ?
+        'In the fields below, please provide the information for your <b>current employer(s)</b> only.'
+        : 'In the fields below, please provide the relevant information for <b>each past employer</b> where you had direct contact with children.';
     return data;
     /*
     var educatorID = req.user.LinkedEducatorID;
