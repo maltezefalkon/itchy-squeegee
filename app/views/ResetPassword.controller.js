5e0b7ab76975da4186f5fabd9a8f91dc7ab8bf81
maltezefalkon <maltezefalkon@gmail.com>
2016-01-03 16:30:15 -0500

    Extra files to support password reset functionality

---
+++ app/views/ResetPassword.controller.js
@@ -0,0 +1,37 @@
+﻿"use strict"
+
+function UserSignupController($scope, $http, $location) {
+    var dict = parseQueryString();
+    $scope.Message = dict.Message;
+    
+    // expose public functions
+    $scope.submitForm = function ($event) {
+        SubmitForm($scope, $http, $event);
+    }
+
+    $scope.hasLowercase = function (value) {
+        return !value || Boolean(value.match(/[a-z]/));
+    }
+
+    $scope.hasUppercase = function (value) {
+        return !value || Boolean(value.match(/[A-Z]/));
+    }
+
+    $scope.hasNumber = function (value) {
+        return !value || Boolean(value.match(/[0-9]/));
+    }
+
+    $scope.confirmMatch = function (value, compare) {
+        return !value || value == compare;
+    }
+
+    $scope.minimumLength = function (value) {
+        return !value || value.length >= 6;
+    }
+}
+
+function SubmitForm($scope, $http, $event) {
+    if (!$event.$valid) {
+        $event.preventDefault();
+    }
+}
---
+++ app/views/ResetPassword.data.js
@@ -0,0 +1,20 @@
+﻿var ViewData = require('./Base.data.js');
+var api = require('../../modules/api');
+var myUrl = require('../../modules/myurl');
+
+module.exports = function (req, userID) {
+    var resetID = req.query.reset;
+    var ret = new ViewData(req, 'Reset Password', 'ResetPassword');
+    return api.querySingle('User', [], null, { UserID: userID }).then(function (user) {
+        if (!user) {
+            ret.fatalError = 'User not found';
+        } else if (user.PasswordResetID != resetID) {
+            ret.fatalError = 'Invalid password reset token';
+        } else {
+            ret.Username = user.UserName;
+            ret.UserID = user.UserID;
+            ret.ResetID = resetID;
+        }
+        return ret;
+    });
+}
